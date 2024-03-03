ad4c9e6f71a5158623ab4062516153cd9872a3bf
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-03 19:10:17 +0300

    add

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ document.querySelector(".btn").onmouseout=function(){
 document.querySelector(".btn").onclick=function(){
 const tcno = document.querySelector("#tcno").value;
     if (tcno.length !== 11) {
+        
         alert("lütfen tc numaranızı 11 haneli olarak giriniz!")
     }else if(tcno[0] === "0") {
         alert("lütfen başında sıfır olmadan tc numaranızı giriniz!")
