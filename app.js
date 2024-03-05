e38a06dbaefb8325aef5976b9dcc0cef60e5efbe
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-05 17:45:51 +0300

    add

--- app.js
+++ app.js
@@ -1,24 +1,74 @@
-document.querySelector(".btn").onmouseover=function(){
-    document.querySelector(".btn").style.color="black";
-}
-
-document.querySelector(".btn").onmouseout=function(){
-    document.querySelector(".btn").style.color="white";
-}
+// selectors
 
 
-document.querySelector(".btn").onclick=function(){
-const tcno = document.querySelector("#tcno").value;
-    if (tcno.length !== 11) {
-        
-        alert("lütfen tc numaranızı 11 haneli olarak giriniz!")
-    }else if(tcno[0] === "0") {
-        alert("lütfen başında sıfır olmadan tc numaranızı giriniz!")
-    }
-    else{
-        alert("başarılı bir giriş yaptınız...") 
+const input = document.getElementById("ID")
+
+const check = document.querySelector(".btn ")
+
+
+
+const checkappend = document.querySelector(".checkıd")
+
+
+
+
+
+// sayfa yenilendiğinde domcontentloaded ile input olan yere fokuslansın kullanıcıya kılaylık açısından  
+document.addEventListener("DOMContentLoaded", () => {
+    input.focus()
+
+// tc kimlik 11 haneli olmlaı
+
+check.addEventListener("click", () => {
+    if (input.value.length != 11 ) {
+        if(checkappend.children.length>0){
+            checkappend.removeChild()
+        }
+
+        const uyarı = document.createElement("p")
+        uyarı.textContent = "Lütfen 11 haneli tc numaranızı giriniz.."
+        checkappend.appendChild(uyarı)
+        uyarı.style.backgroundColor = "yellow"
+        uyarı.style.fontSize = "12px"
+        input.value = ""
+        input.focus()
        
-    }
-  }
 
+    }else {
+
+        const uyarı = document.createElement("p")
+        uyarı.textContent = " Tebrikler başarılı bir giriş yaptınız..."
+        checkappend.appendChild(uyarı)
+        uyarı.style.backgroundColor = "green"
+        uyarı.style.fontSize = "12px"
+        input.value = ""
+        input.focus()
+    }
+})
+
+})
+
+// check butonunun mouse üzerine geldiğinde  ve ayrıldığında yapılacak işlemler 
+// onmouseover-->üzerine gelindiğinde 
+
+check.addEventListener("mouseover", () => {
+    check.style.color = "black"
+    check.style.cursor = "pointer"
+    check.style.fontSize = "20px"
+
+})
+// onmouseout--> üzerinden ayrıldığında 
+
+check.addEventListener("mouseout", () => {
+    check.style.color = "white"
+    check.style.fontSize = "16px"
+})
+
+
+//  eklediğimiz p elemnetinin silinmesi
+
+input.addEventListener("change",()=>{
+   if(checkappend.children.length>0) {
+    document.querySelector(".checkıd p").remove()}
+})
 
